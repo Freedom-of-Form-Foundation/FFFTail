45df27f57405811f18f6fc97d0e99205b2111275
T&R <bottest9@gmail.com>
2023-10-12 18:09:18 -0500

    start of grab rework for bugfix
    started work on fastRead() and fastDecode() which should be able to be multi-processed and speed up graphing as well as avoid misalignments in real time (hopefully); only have the barebones of both functions.

--- Interpreter/speedtest.py
+++ Interpreter/speedtest.py
@@ -19,86 +19,12 @@ import struct
 
 #serial stuff
 ser = serial.Serial(baudrate=230400, timeout=None) #serial class
-#ser.set_buffer_size(rx_size = 12800, tx_size = 12800) <= try messing around with buffer size
+#ser.set_buffer_size(rx_size = 256, tx_size = 256) #defualot buffer size is 2^16 =65536 #doesn't seem to do anything
 ser.port = 'COM3' #<<<<======== set the port IMPORTANT!!!! CHANGE THIS AS NEEDED!!!!!!!
 #ser.setDTR(False) #this line makes the serial data readablle
 #ser.setRTS(False) #this line makes the serial data readablle
 
 
-
-#read 1 input line from the serial
-#helps make sure everything is in sync
-z = b''
-def read():
-    global z
-    rc = 0 #return counter to make sure we do three at a time
-    #we're trying to read ~19 bytes from serial
-    #at 1024 samples:
-    #- read(19) ~1.047
-    #- read(1)+parse ~ 1.5 for 1/3; 4.6 for equivalent of 1024
-    #- read_until(b'\n') = ~4.6
-    #- readline = ~4.5
-    '''From the results of these tests we can conclude that the best way forward is going to be read(n) and making sure we are sending the correct # of bytes at once'''
-    
-    #z = y[:-1].split(",") #[time, raw, env]
-    while rc < 3:
-        y = ser.read(12)
-        if (y == b'\n') or (y ==b','):
-            x = z
-            z = b''
-            #print(x)
-            rc = rc + 1
-            return x
-        else:
-            z = z+y
-    #return z
-
-#utf-16 should be the correct encoding for how we're sending it from arduino
-#however I get random chars when I decode that so I think it's a scyn issue
-#since we know we're looking for a message 12 characters long as long as we have a window (w) twice that size we should find the correct orientation by sliding accross it
-#this code probabl won't be useful beyond these tests but who knows, here's to hoping!
-def testEncoding():
-    #printing the array, or segments of it gives a value such as: b'\x01\xf3\xe5\xc8\xf9\xe0\x8f\xd1IV&F\x1d+E\xe4+E\xab/Er1E'
-    #printing just an individual byte gives us a value from 0-255
-    w = ser.read(24)
-    print("messing around with w[n]")
-    for i in range(len(w)-8):
-        p = []
-        j = i+4
-        #print(w[i:j].decode('utf-16'))
-        p = w[i:j]
-        print(p[0])
-    print("just print(w): ")
-    print(w)
-
-#data storage for our graphs
-#might merge this with the buffer eventually, but for now we're keeping it seprate
-xs = [] #time
-ys = [] #raw values
-#y2s = [] #env values
-lastcall = 0; #to try to help kep track of when we're calling the code to store stuff in the buffer
-#the function which animates things
-def roughBuff():
-    #make sure we're storing to the right arays
-    global xs
-    global ys
-    global lastcall
-    #rouighly one tenth of our tample rate (1024) so swe should call this every 10th of a second
-    #for i in range(1024):
-    data = read() #read once from serial
-    #print(data) #debugging stuff
-    if len(z) > 2: #this makes sure we only record valid arrays
-        if (z[0] != '') and (z[0] != '') and (z[0] != ''):
-            #time = timecheck(int(data[0])) #convert out time to be poper for the graph; MIGHT BE REDUNDANT WITH NEW GRAPH?
-            #add data to our axis
-            xs.append(z[0]) 
-            ys.append(z[1])
-            #DO NOT DELETE THIS LINE
-            #dump = data[2] #for some reason if you don't have this line it gives and out of bounds error on the previous line
-    #print("ran rough buff at: {:f}".format(time.time()))
-    lastcall = time.time()
-
-'''this function works'''
 #function we should call to try and make things in sync
 #calling it pawshake instead of handshake because it's funny and furry
 def pawshake():
@@ -126,57 +52,8 @@ def pawshake():
         #if we did the shake we don't need to wait
         if not shake:
             time.sleep(.1) #sleep for .1 seconds before checking to see if theer was a response again
-                
-#attempt at making the program read data fast and not have it be weirdly segmented
-def smartRead():
-    #we know the incoming message should be 8 bytes
-    #1 x uint32_t + 2 x uint16_t = 4 + 2*2 = 8
-    #[0-3] = time of sample
-    #[4-5] = raw value
-    #[6-7] = env value
-    
-    #essentially check to make sure we have completed messages in there
-    #need to implement some way of making sure we're always looking for the right amount of bytes
-    if(ser.in_waiting % 12 == 0):
-        #read one whole output
-        m = ser.read(32)
-        #need to decide if this is how I actually want to return values
-        #does conversion/calculation need to be done elsewhere for speed?
-        t = m[0:3]#.decode()
-        r = m[4:5]#.decode()
-        e = m[6:7]#.decode()
-    print("Oops! Not done yet!")
-    print(type(m))
-    print("m:")
-    for i in m:
-        print("{j}".format(j=i))
-    print("Time: {ti} \nRaw: {ra} \nEnv: {en}".format(ti=t, ra=r, en=e))
-    print("Decoded - Time: {ti} \nRaw: {ra} \nEnv: {en}".format(ti=t.decode('utf-32'), ra=r.decode('utf-16'), en=e.decode('utf-16')))
 
 
-#grab multiple same length samples and check to see if they remain aligned each time
-passes = [] #for storing data
-def alignCheck(pcnt):
-    print("Prefomring Alignment check...")
-    #for keeping track of which pass we're on
-    #ocnt = pcnt
-    while pcnt > 0:
-        if ser.in_waiting > 24:
-            m = ser.read(24)
-            print("Successfully read {b} bytes from in_waiting!".format(b=len(m)))
-            passes.append(m)
-            pcnt = pcnt-1
-
-    #just to make things look nicer/make the output clearer
-    print("{c} passes of length {l}:".format(c=len(passes), l=len(passes[0])))
-    #print and compare them all:
-    for i in range(len(passes[0])):
-        toPrint = ""
-        for j in range(len(passes)):
-            toPrint += "{b}\t".format(b=passes[j][i])
-        print(toPrint)
-
-'''this function works'''
 #variation of align checker for use with SerialSpeedTest-USB2
 #in short: it looks for this pattern: [prev entry], time[4 bytes], raw [2 bytes], env [2 bytes], time [4 bytes], [new entry]
 #each sample is expected to be 12 bytes, with the 4 at the start and end being thwe time of the sample
@@ -245,6 +122,44 @@ def timeAlignCheck(scnt, v):
         print("Test Failed, Expected pattern not found :( :(")
         #return the step so we can use it to measure alignment
 
+serial_record = ''
+def fastRead():
+    while ser.in_waiting:
+         serial_record+=ser.read(ser.in_waiting)
+         #delay for a lil bit
+         time.sleep(0.01) #wait for about 10ms or so to give the buffer time to fill up a bit more then add it to our record
+
+def fastDecode(alightnment, packsize):
+    tbr = []
+    #only start decoding if there is 10 samples at least, this will help with checking error correction
+    #additionally 10 packets should be sent in ~ .00978 seconds which is still 1/3 of the time it takes minimum between frames;
+    #if we want we can adjust this later
+    if serial_record > packsize * 10:
+        for i in range(alignment, (len(todecode) - packsize), packsize): 
+            e = i + packsize
+            #slice up our sample into the values we want
+            sample = todecode[i:e]
+            #maybe redo these to be sample size independent when you get the chance
+            #DOUBLE CHECK THIS!!!!!!!!!!!! could be slicing wrong if we get the wrong values
+            timebytes = sample[0:4]
+            rawbytes = sample[4:6] 
+            envbytes = sample[6:8]
+            #do the math and get our data
+            timeact = fixVals4(timebytes)
+            rawact = fixVals2(rawbytes)
+            envact = fixVals2(envbytes)
+
+            #if our raw and env values are wayyy out of the expected range then it's likely to be a misalignment
+            #figure out how to simplify or statement
+            if((rawact > 4096 or rawact < 0) or (envactct > 4096 or envact < 0)):
+                return "misalign likely"
+            #add our sample to what we want to return
+            tbr.append([timeact, rawact, envact])
+
+    #only return the values when they're good
+    #increment a value so we remember where we are in serial_record 
+    return tbr
+
 #return numpoints number of samples by reading from serial
 ''' buff and m keep getting one entry longer every call at a time and I'm not sure why'''
 #likely has to do with the passes code calling it
@@ -264,6 +179,7 @@ def grabData(passes, packsize):
     #buff = buff[-60:] #limit the size of the buffer
     return buff
 
+#used to decode bytes
 def grabDecode(todecode, alignment, packsize):
     tbr = []
     #since we want to iterate from the start of our alignment to the end our our decode buffer where i should be multiples of the size of our packets
@@ -283,9 +199,10 @@ def grabDecode(todecode, alignment, packsize):
         rawact = fixVals2(rawbytes)
         envact = fixVals2(envbytes)
 
+        if(rawact)
         #add our sample to what we want to return
         tbr.append([timeact, rawact, envact])
-
+        
     #return our values
     return tbr
             
@@ -422,35 +339,159 @@ def niceTicks(data):
 #-------------------------        
 # ACTUALLY RUN EVERYTHING
 #-------------------------
-print("starting!")
-ser.open() #open it
-pawshake()
-#smartRead()
-#alignCheck(8)
-alignment = timeAlignCheck(8, True)
-#etime = time.time() + 30
-'''attempt at live graphing'''
-test_graph = animation.FuncAnimation(fig, graphTest, fargs=(102, alignment, 12), interval = 30)
-plt.show()
+if __name__ == "__main__":
+    print("starting!")
+    ser.open() #open it
+    pawshake()
+    #smartRead()
+    #alignCheck(8)
+    alignment = timeAlignCheck(8, True)
+    #etime = time.time() + 30
+    '''attempt at live graphing'''
+    test_graph = animation.FuncAnimation(fig, graphTest, fargs=(102, alignment, 12), interval = 30)
+    plt.show()
 
 
+    '''
+    #non-live graphing
+    graphTest(2048, alignment, 12)
+    plt.show()
+
+    print("about to grab!")
+    start = time.time()
+    bytedata = grabData(2048, 12)
+    end = time.time()
+    total = end - start
+    data = grabDecode(bytedata, alignment, 12)
+    expected = (data[-1][0] - data[0][0]) / (10**6) #need to do a microsecond to second conversion
+    print("Here's the data: {c} samples grabbed in {s} seconds".format(c=len(data), s=total))
+    print("python time vs sample time: ")
+    print("p: {t1}".format(t1=total))
+    print("s: {t2}".format(t2=expected))
+    print("data can be accessed as 'data'")
+    '''
+    print("Closing serial port!")
+    ser.close()
+
+#old code that I will delete later
 '''
-#non-live graphing
-graphTest(2048, alignment, 12)
-plt.show()
+#read 1 input line from the serial
+#helps make sure everything is in sync
+z = b''
+def read():
+    global z
+    rc = 0 #return counter to make sure we do three at a time
+    #we're trying to read ~19 bytes from serial
+    #at 1024 samples:
+    #- read(19) ~1.047
+    #- read(1)+parse ~ 1.5 for 1/3; 4.6 for equivalent of 1024
+    #- read_until(b'\n') = ~4.6
+    #- readline = ~4.5
+    #From the results of these tests we can conclude that the best way forward is going to be read(n) and making sure we are sending the correct # of bytes at once
+    
+    #z = y[:-1].split(",") #[time, raw, env]
+    while rc < 3:
+        y = ser.read(12)
+        if (y == b'\n') or (y ==b','):
+            x = z
+            z = b''
+            #print(x)
+            rc = rc + 1
+            return x
+        else:
+            z = z+y
+    #return z
 
-print("about to grab!")
-start = time.time()
-bytedata = grabData(2048, 12)
-end = time.time()
-total = end - start
-data = grabDecode(bytedata, alignment, 12)
-expected = (data[-1][0] - data[0][0]) / (10**6) #need to do a microsecond to second conversion
-print("Here's the data: {c} samples grabbed in {s} seconds".format(c=len(data), s=total))
-print("python time vs sample time: ")
-print("p: {t1}".format(t1=total))
-print("s: {t2}".format(t2=expected))
-print("data can be accessed as 'data'")
+#attempt at making the program read data fast and not have it be weirdly segmented
+def smartRead():
+    #we know the incoming message should be 8 bytes
+    #1 x uint32_t + 2 x uint16_t = 4 + 2*2 = 8
+    #[0-3] = time of sample
+    #[4-5] = raw value
+    #[6-7] = env value
+    
+    #essentially check to make sure we have completed messages in there
+    #need to implement some way of making sure we're always looking for the right amount of bytes
+    if(ser.in_waiting % 12 == 0):
+        #read one whole output
+        m = ser.read(32)
+        #need to decide if this is how I actually want to return values
+        #does conversion/calculation need to be done elsewhere for speed?
+        t = m[0:3]#.decode()
+        r = m[4:5]#.decode()
+        e = m[6:7]#.decode()
+    print("Oops! Not done yet!")
+    print(type(m))
+    print("m:")
+    for i in m:
+        print("{j}".format(j=i))
+    print("Time: {ti} \nRaw: {ra} \nEnv: {en}".format(ti=t, ra=r, en=e))
+    print("Decoded - Time: {ti} \nRaw: {ra} \nEnv: {en}".format(ti=t.decode('utf-32'), ra=r.decode('utf-16'), en=e.decode('utf-16')))
+
+#utf-16 should be the correct encoding for how we're sending it from arduino
+#however I get random chars when I decode that so I think it's a scyn issue
+#since we know we're looking for a message 12 characters long as long as we have a window (w) twice that size we should find the correct orientation by sliding accross it
+#this code probabl won't be useful beyond these tests but who knows, here's to hoping!
+def testEncoding():
+    #printing the array, or segments of it gives a value such as: b'\x01\xf3\xe5\xc8\xf9\xe0\x8f\xd1IV&F\x1d+E\xe4+E\xab/Er1E'
+    #printing just an individual byte gives us a value from 0-255
+    w = ser.read(24)
+    print("messing around with w[n]")
+    for i in range(len(w)-8):
+        p = []
+        j = i+4
+        #print(w[i:j].decode('utf-16'))
+        p = w[i:j]
+        print(p[0])
+    print("just print(w): ")
+    print(w)
+
+#data storage for our graphs
+#might merge this with the buffer eventually, but for now we're keeping it seprate
+xs = [] #time
+ys = [] #raw values
+#y2s = [] #env values
+lastcall = 0; #to try to help kep track of when we're calling the code to store stuff in the buffer
+#the function which animates things
+def roughBuff():
+    #make sure we're storing to the right arays
+    global xs
+    global ys
+    global lastcall
+    #rouighly one tenth of our tample rate (1024) so swe should call this every 10th of a second
+    #for i in range(1024):
+    data = read() #read once from serial
+    #print(data) #debugging stuff
+    if len(z) > 2: #this makes sure we only record valid arrays
+        if (z[0] != '') and (z[0] != '') and (z[0] != ''):
+            #time = timecheck(int(data[0])) #convert out time to be poper for the graph; MIGHT BE REDUNDANT WITH NEW GRAPH?
+            #add data to our axis
+            xs.append(z[0]) 
+            ys.append(z[1])
+            #DO NOT DELETE THIS LINE
+            #dump = data[2] #for some reason if you don't have this line it gives and out of bounds error on the previous line
+    #print("ran rough buff at: {:f}".format(time.time()))
+    lastcall = time.time()
+
+#grab multiple same length samples and check to see if they remain aligned each time
+passes = [] #for storing data
+def alignCheck(pcnt):
+    print("Prefomring Alignment check...")
+    #for keeping track of which pass we're on
+    #ocnt = pcnt
+    while pcnt > 0:
+        if ser.in_waiting > 24:
+            m = ser.read(24)
+            print("Successfully read {b} bytes from in_waiting!".format(b=len(m)))
+            passes.append(m)
+            pcnt = pcnt-1
+
+    #just to make things look nicer/make the output clearer
+    print("{c} passes of length {l}:".format(c=len(passes), l=len(passes[0])))
+    #print and compare them all:
+    for i in range(len(passes[0])):
+        toPrint = ""
+        for j in range(len(passes)):
+            toPrint += "{b}\t".format(b=passes[j][i])
+        print(toPrint)
 '''
-print("Closing serial port!")
-ser.close()
